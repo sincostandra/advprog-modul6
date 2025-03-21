4e24b3b046828f4c70306e9849302a9b5a33847c
sincostandra <utandraemail@gmail.com>
2025-03-21 20:51:35 +0700

    (Bonus) Function improvement + Reflection

--- src/lib.rs
+++ src/lib.rs
@@ -11,30 +11,30 @@ pub struct ThreadPool {
 type Job = Box<dyn FnOnce() + Send + 'static>;
 
 impl ThreadPool {
-    // --snip--
-
-    pub fn new(size: usize) -> ThreadPool {
-        assert!(size > 0);
+    /// Creates a new ThreadPool instance if the provided size is greater than zero.
+    /// Returns an error otherwise.
+    pub fn build(size: usize) -> Result<ThreadPool, &'static str> {
+        if size == 0 {
+            return Err("Pool size must be greater than zero.");
+        }
 
         let (sender, receiver) = mpsc::channel();
-
-        let receiver: Arc<Mutex<mpsc::Receiver<Job>>> = Arc::new(Mutex::new(receiver));
-
-        let mut workers: Vec<Worker> = Vec::with_capacity(size);
+        let receiver = Arc::new(Mutex::new(receiver));
+        let mut workers = Vec::with_capacity(size);
 
         for id in 0..size {
             workers.push(Worker::new(id, Arc::clone(&receiver)));
         }
 
-        ThreadPool { workers, sender }
+        Ok(ThreadPool { workers, sender })
     }
 
+    /// Executes a task by sending it to the thread pool.
     pub fn execute<F>(&self, f: F)
     where
         F: FnOnce() + Send + 'static,
     {
         let job = Box::new(f);
-
         self.sender.send(job).unwrap();
     }
 }
@@ -44,13 +44,11 @@ struct Worker {
     thread: thread::JoinHandle<()>,
 }
 
-// --snip--
 impl Worker {
     fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
         let thread = thread::spawn(move || {
             while let Ok(job) = receiver.lock().unwrap().recv() {
                 println!("Worker {id} got a job; executing.");
-
                 job();
             }
         });
